281e0c62f6dd1b7c22ba0fd241937aaf04576cff
COR1999 <cian.orourke@gmail.com>
2020-01-24 19:03:59 +0000

    fixed page reload bug

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
+
+
+
 $(document).ready(function () {
-    $('#search').click(function () {
 
-        var token = 'BQDwMEpdy8VHd-m0uAiaPni_-v2i3G7sY1c1ErvIq5d4LCTff5obN6admcBupZ0FdkxXJAZwJoTcHFwGlKgVfYqr1ttqTLcmK12bdEpmv6ffmYk7bPLQq-CC-cXPuUzbf0nOS8m7iRkBhC6DC7_hq-F-RBv14WxfzrY'
+    $("#search").on("click", searchApi);
+    // Get the input field
+    document.getElementById("myInput").addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("search").click();
+        }
+    });
 
-        $.ajax({
-            url: 'https://api.spotify.com/v1/search?type=artist&query=' + $("#myInput").val(),
-            headers: {
-                Authorization: 'Bearer ' + token
-            },
-        })
-            .then(function (oData) {
-                var artistId = oData.artists.items[0].id;
-                $.ajax({
-                    url: 'https://api.spotify.com/v1/artists/' + artistId + '/top-tracks?country=IE',
-                    headers: {
-                        Authorization: 'Bearer ' + token
-                    },
-                }).then(function (response) {
-                    let songName = []
-                    let playNow = []
-                    response.tracks.forEach(function (album) {
-                        songName.push("<li>" + album.name + '<a href="' + album.external_urls.spotify + '" target="_blank"> Play Now</a></li>');
-                        // playNow.push('<a href="' + album.external_urls.spotify + '">Play Now</a>');
-                        console.log(album.external_urls.spotify);
-                    })
-                    $('#artist-songs').html(songName);
-                    // $('#artist-playNow').html(playNow);
-
-                });
-                let artistName = oData.artists.items[0].name;
-                $('#artist-name').html(`<h2>${artistName}</h2>`)
-
-                let artistImage = oData.artists.items[0].images[1].url;
-                $('#artist-image').html(`<img src="${artistImage}">`)
-            })
-
-    })
-})
-
-
+});
 function refreshToken(token) {
 
 }
 
 
+
+
+function searchApi() {
+    var token = 'BQDVW29UVjySr9_XvUk_dh07K3tquAily4LEI2H8Z__IVmlp9pOdTa_bC2kZXzeVcyC2DkTKAmCgTG_WDwjnim6ZEBabPe7KNo_7oc9k57Uz2a6Inhm2UXyflcRS32Ji5TqEWXVilgjUnEf49N67Lu8U-iFJJ7mDaRg'
+
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search?type=artist&query=' + $("#myInput").val(),
+        headers: {
+            Authorization: 'Bearer ' + token
+        },
+    })
+        .then(function (oData) {
+            var artistId = oData.artists.items[0].id;
+            $.ajax({
+                url: 'https://api.spotify.com/v1/artists/' + artistId + '/top-tracks?country=IE',
+                headers: {
+                    Authorization: 'Bearer ' + token
+                },
+            }).then(function (response) {
+                let songName = []
+                let playNow = []
+                response.tracks.forEach(function (album) {
+                    songName.push("<li>" + album.name + '<a href="' + album.external_urls.spotify + '" target="_blank"> Play Now</a></li>');
+                    // playNow.push('<a href="' + album.external_urls.spotify + '">Play Now</a>');
+                    console.log(album.external_urls.spotify);
+                })
+                $('#artist-songs').html(songName);
+                // $('#artist-playNow').html(playNow);
+
+            });
+            let artistName = oData.artists.items[0].name;
+            $('#artist-name').html(`<h2>${artistName}</h2>`)
+
+            let artistImage = oData.artists.items[0].images[1].url;
+            $('#artist-image').html(`<img src="${artistImage}">`)
+        })
+}
 
